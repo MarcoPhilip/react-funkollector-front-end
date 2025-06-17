c609cd5705bb81b4297c8a8dabfe5e8a3ffe2f09
marcophilip <marcophilipagapito@gmail.com>
2025-06-17 15:16:43 -0700

    funkoServices pseudo coded. diverting to sigp up functionality

---
+++ src/services/funkoService.js
@@ -0,0 +1,22 @@
+
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/funkos`;
+
+
+// Create the service function called index
+
+
+
+
+
+// Create the service function called create
+
+
+
+
+// Create the service function called deleteFunko
+
+
+
+
+// Export all functions above
