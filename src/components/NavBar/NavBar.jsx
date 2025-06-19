c0fd70c419a698dbefca35c77a5e4c8bb08ee291
marcophilip <marcophilipagapito@gmail.com>
2025-06-18 21:55:00 -0700

    useContext applied in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,28 @@
 // src/components/NavBar/NavBar.jsx
 import { Link } from 'react-router';
+// Import the useContext hook
+import { useContext } from 'react';
+
+// Import the UserContext object
+import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
-  return (
-     <nav>
-      <ul>
-        <li><Link to='/sign-up'>Sign Up</Link></li>
-      </ul>
-    </nav>
-  );
+
+    const { user } = useContext(UserContext);
+
+    return (
+        <nav>
+            {user ? (
+                <ul>
+                    <li>Welcome, {user.firstname}</li>
+                </ul>
+            ) : (
+                <ul>
+                    <li><Link to='/sign-up'>Sign Up</Link></li>
+                </ul>
+            )}
+        </nav>
+    );
 };
 
 export default NavBar;
