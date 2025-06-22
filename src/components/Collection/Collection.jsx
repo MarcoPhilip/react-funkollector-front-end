0ad3fa57f2e02ebde8799d0b143442408800dc42
marcophilip <marcophilipagapito@gmail.com>
2025-06-21 17:15:52 -0700

    collection and wishlist services files are defined. started working on the collection component.

---
+++ src/components/Collection/Collection.jsx
@@ -0,0 +1,20 @@
+
+// Imports if necessary
+
+
+
+
+// Define the Collection function
+const Collection = () => {
+    // Use state for collection
+    
+    // Define the useEffect
+
+    // Rendering
+};
+
+
+
+
+
+// Export Collection
---
+++ src/services/collectionService.js
@@ -0,0 +1,64 @@
+
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/collections`;
+
+
+// Create the service function for collectionIndex
+const collectionIndex = async () => {
+    const token = localStorage.getItem('token');
+    try {
+        const res = await fetch(BASE_URL, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+
+// Create the service function for addToCollection 
+const addToCollection = async (funkoId) => {
+    const token = localStorage.getItem('token');
+
+    try {
+        const res = await fetch(`${BASE_URL}/${funkoId}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
+    
+};
+
+
+
+// Create the service function for removeFromCollection
+const removeFromCollection = async (funkoId) => {
+    const token = localStorage.getItem('token');
+    try {
+        const res = await fetch(`${BASE_URL}/${funkoId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+
+// Export all functions above
+export {
+    collectionIndex,
+    addToCollection,
+    removeFromCollection
+}
